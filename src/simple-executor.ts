aafda5bc346d5b0faa222f855e3344916c519036
Kasama Chenkaow <kasama.chenkaow@gmail.com>
2024-08-11 12:17:57 +0200

    feat(simple-executor): simplify children type

--- src/simple-executor.ts
+++ src/simple-executor.ts
@@ -3,21 +3,23 @@ type Task<TArgs> = (args: TArgs) => Promise<void>
 type TaskNode<TArgs> = {
   task: Task<TArgs>
   input: TArgs
-  children: TaskNode<unknown>[]
+  children?: TaskNode<unknown>[]
 }
 
 export const addTaskChild = <TArgs, TChild>(
   parent: TaskNode<TArgs>,
   child: TaskNode<TChild>,
 ) => {
+  if (!parent.children) {
+    parent.children = []
+  }
+
   parent.children.push(child as TaskNode<unknown>)
 }
 
 export const simpleDagsExecutor = async <TArgs>(taskNode: TaskNode<TArgs>) => {
-  const pendingChildren = taskNode.children
-
-  if (pendingChildren.length !== 0) {
-    await Promise.all(pendingChildren.map(simpleDagsExecutor))
+  if (taskNode.children && taskNode.children.length !== 0) {
+    await Promise.all(taskNode.children.map(simpleDagsExecutor))
   }
 
   await taskNode.task(taskNode.input)
